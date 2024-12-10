9a5d8bcc06759822b71208f19a1251548d35f699
Muhammad Ishfaq <ishfaqcompk6@gmail.com>
2024-12-10 13:40:59 +0500

    More Custom Control

--- app/src/main/java/com/example/myapplication/AdsEntryManager.kt
+++ app/src/main/java/com/example/myapplication/AdsEntryManager.kt
@@ -1,16 +1,17 @@
 package com.example.myapplication
 
 import android.content.Context
+import android.os.Bundle
 import com.funn.ads.domain.model.ControllerModel
 import com.funn.ads.sdk.FunAdsSdk
 import com.funn.ads.sdk.init
+import com.monetization.core.ad_units.core.AdType
 import com.monetization.core.listeners.ControllersListener
 
 object AdsEntryManager {
 
     fun initControllers(context: Context, onInitialized: () -> Unit) {
         val controllersListener = object : ControllersListener {
-
         }
         FunAdsSdk.init(
             context = context,
